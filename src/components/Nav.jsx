a985a734d98545cd03567549c4da5e65d4753cad
Arthur Bobadilla Franchi <arthurbobadilla2006@gmail.com>
2024-09-21 13:31:52 -0300

    Add: css files

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import { NavStyle } from "../css/NavStyle"
 
 const Nav = () => {
     return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import GlobalStyle from "./css/GlobalStyle.jsx"
 import App from './App.jsx'
 import Home from './routes/Home.jsx'
 import Login from './routes/Login.jsx'
