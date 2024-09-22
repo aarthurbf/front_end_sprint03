51bd7290bb0d8415a13be6fa303287390cc90d12
Arthur Bobadilla Franchi <arthurbobadilla2006@gmail.com>
2024-09-22 12:37:26 -0300

    Fix: form

--- src/css/SelectionStyle.jsx
+++ src/css/SelectionStyle.jsx
@@ -31,19 +31,19 @@ export const SelectionStyle = styled.section`
   .selection-title {
     color: var(--color3);
     font-size: 4rem;
-    margin: 5rem 0 0 15rem;
+    margin: 4rem 0 0 10rem;
   }
 
   .selection-text {
     color: var(--color1);
-    font-size: 1.4rem;
-    margin: 2rem 6rem 0 15rem;
+    font-size: 1.3rem;
+    margin: 1rem 6rem 0rem 10rem;
   }
 
   .form {
-    width: 500px;
+    width: 550px;
     height: 450px;
-    background: var(--color6);
+    background: var(--color7);
     border-radius: 1rem;
     box-shadow: 0 0 3rem -10px var(--color1);
     margin: calc(50vh - 220px) auto;
@@ -54,7 +54,7 @@ export const SelectionStyle = styled.section`
   .form h2 {
     margin: 0.6rem 0;
     padding-bottom: 0.6rem;
-    width: 100%;
+    width: 28%;
     color: var(--color3);
     border-bottom: 3px solid var(--color2);
   }
@@ -65,32 +65,34 @@ export const SelectionStyle = styled.section`
     outline: none;
     border: 0;
     transition: all 0.3s;
-    border-bottom: 2px solid var(--color2);
+    border: 2px solid var(--color6);
+    border-radius: 0.8rem
   }
   .form input:focus {
-    border-bottom: 2px solid var(--color4);
+    border: 2px solid var(--color4);
   }
-
   .form p:before {
     content: attr(type);
     display: block;
-    margin: 2rem 0 0;
+    margin: 2rem 0.2rem 0.2rem;
     font-size: 0.9rem;
-    color: var(--color1);
+    color: var(--color2);
     text-transform: uppercase;
+    font-weight: 650;
   }
   .form button {
     float: right;
     padding: 1rem 1rem;
     margin: 1rem 0 0;
-    border: 2px solid var(--color2);
+    border: 2px solid var(--color5);
     background: transparent;
     color: var(--color2);
     cursor: pointer;
     transition: all 0.3s;
+    border-radius: 0.8rem;
   }
   .form button:hover {
-    background: var(--color2);
+    background: var(--color4);
     color: var(--color6);
   }
 `;
