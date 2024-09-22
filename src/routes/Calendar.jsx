8fef79e3616f7ec7b8828661d764c6eddb758eaf
Arthur Bobadilla Franchi <arthurbobadilla2006@gmail.com>
2024-09-22 17:15:20 -0300

    Add: Calenda Page

--- src/routes/Calendar.jsx
+++ src/routes/Calendar.jsx
@@ -1,10 +1,13 @@
+import { CalendarStyle } from '../css/CalendarStyle';
+import Tabela from '../assets/images/tabela.png';
 
 const Calendar = () => {
     return (
-        <>
-            <h1>Calendar</h1>
-        </>
-    )
-}
+        <CalendarStyle>
+            <h1 className='calendar-title'>Calendário Formula E - 2024</h1>
+            <img src={Tabela} alt="Tabela das corridas" className='calendar-image' />
+        </CalendarStyle>
+    );
+};
 
-export default Calendar
+export default Calendar;
