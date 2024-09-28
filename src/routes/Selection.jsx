854b5997916f890e3431e0268ba2d353f2fc2476
Arthur Bobadilla Franchi <arthurbobadilla2006@gmail.com>
2024-09-28 11:48:17 -0300

    Add: Talento E

--- src/routes/Selection.jsx
+++ src/routes/Selection.jsx
@@ -8,9 +8,9 @@ const Selection = () => {
                     <h2>Em busca do próximo campeão da Fórmula E</h2>
                 </div>
                 <div className="selection-container">
-                    <h1 className="selection-title">Seleção especial</h1>
+                    <h1 className="selection-title">Talento E</h1>
                     <p className="selection-text">
-                        Transformando sonhos em realidade, nosso projeto oferece uma seleção especial para jovens brasileiros se tornarem pilotos de Fórmula E. Ao serem selecionados, esses talentos terão a oportunidade de se tornarem ídolos nacionais e internacionais, elevando o nome do Brasil no cenário automobilístico mundial.
+                        Transformando sonhos em realidade, nosso projeto Talento E oferece uma seleção especial para jovens brasileiros se tornarem pilotos de Fórmula E. Ao serem selecionados, esses talentos terão a oportunidade de se tornarem ídolos nacionais e internacionais, elevando o nome do Brasil no cenário automobilístico mundial.
                         <br /><br />
                         Esta iniciativa visa não apenas descobrir novos talentos, mas também popularizar a Fórmula E no Brasil, inspirando uma nova geração de entusiastas e promovendo a sustentabilidade no esporte a motor.
                     </p>
